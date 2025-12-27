f3f92801d2ec779b50582a7643b1cb9a0d8fb383
brandonjoyce2 <brandonj@xternsoftware.com>
2025-12-27 18:11:30 -0500

    Make Organization description nullable

--- src/database/migrations/2025_09_02_120000_make_organization_description_nullable.php
+++ src/database/migrations/2025_09_02_120000_make_organization_description_nullable.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('organizations', function (Blueprint $table) {
-            $table->id();
-            $table->text('name');
-            $table->text('description')->nullable();
-            $table->timestampAudits();
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('organizations');
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->text('description')->nullable(false)->change();
+        });
     }
 };
